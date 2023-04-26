ce34a34239969dc0f3e770a806f72669e80ebc59
xiangang <342005702@qq.com>
2023-04-27 00:34:15 +0800

    feature：增加MySQL数据库连接
    增加application.conf的使用

--- src/main/kotlin/com/nxg/plugins/HTTP.kt
+++ src/main/kotlin/com/nxg/plugins/HTTP.kt
@@ -24,7 +24,7 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
+        allowHeader("KSignaling")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
     routing {
