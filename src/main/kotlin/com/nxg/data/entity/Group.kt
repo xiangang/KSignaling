86cd9e74fa49c75dd2b0b33cf707f8243ad78b44
xiangang <xiangang12202@gmail.com>
2023-05-09 00:18:58 +0800

    feature：增加用户加入群组的接口

--- src/main/kotlin/com/nxg/data/entity/Group.kt
+++ src/main/kotlin/com/nxg/data/entity/Group.kt
@@ -3,3 +3,5 @@ package com.nxg.data.entity
 data class Group(val id: Long, val groupName: String, val creatorId: Long)
 data class GroupMember(val id: Long, val groupId: Long, val userId: Long)
 data class GroupMessage(val id: Long, val groupId: Long, val senderId: Long, val messageContent: String)
+
+data class JoinGroupRequest(val userId: Long)
